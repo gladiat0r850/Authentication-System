dcac2d9cf4dcc63118b6e7241cb5d89d5802b3d3
X3NOGLADIAT0R <game23452332pro@gmail.com>
2026-03-01 23:19:39 +0400

    cors fix

--- backend/app.ts
+++ backend/app.ts
@@ -12,7 +12,7 @@ const app = express()
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ['https://authentication-system-62ef.vercel.app/', 'http://localhost:3000'],
     credentials: true
 }))
 mongoose.connect(process.env.DATABASE_NAME!).catch((err) => {
