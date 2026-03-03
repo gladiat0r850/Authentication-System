42ba691071e4254b6c3bf4cc3a56c2656d43633b
X3NOGLADIAT0R <game23452332pro@gmail.com>
2026-03-03 17:24:46 +0400

    ...

--- backend/app.ts
+++ backend/app.ts
@@ -12,7 +12,7 @@ const app = express()
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-    origin: ['https://authentication-system-imz2.vercel.app', 'http://localhost:3000'],
+    origin: 'https://authentication-system-kwew.vercel.app',
     credentials: true
 }))
 mongoose.connect(process.env.DATABASE_NAME!).catch((err) => {
